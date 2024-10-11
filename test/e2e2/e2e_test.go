ef6dc4369ac3390003384528a4158786a1de79da
John Pitman <jpitman@redhat.com>
2024-10-11 15:50:27 -0400

    improvements
    Signed-off-by: John Pitman <jpitman@redhat.com>

--- test/e2e2/e2e_test.go
+++ test/e2e2/e2e_test.go
@@ -15,7 +15,6 @@
 package e2e2
 
 import (
-	"context"
 	"testing"
 	"time"
 
@@ -28,25 +27,13 @@ import (
 )
 
 type BasicTestSuite struct {
-	suite.Suite
+	fixture.BaseSuite
 }
 
-func (suite *BasicTestSuite) Test_Agent_Managed() {
+func (suite *BasicTestSuite) Test_1() {
 	requires := suite.Require()
 
-	ctx := context.Background()
-
-	config, err := fixture.GetSystemKubeConfig("vcluster-control-plane")
-	requires.Nil(err)
-	controlPlaneClient, err := fixture.NewKubeClient(config)
-	requires.Nil(err)
-
-	config, err = fixture.GetSystemKubeConfig("vcluster-agent-managed")
-	requires.Nil(err)
-	managedAgentClient, err := fixture.NewKubeClient(config)
-	requires.Nil(err)
-
-	// Create a managed application in the control-plane's cluster
+	// Create a managed application in the principal's cluster
 	app := argoapp.Application{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "guestbook",
@@ -70,44 +57,21 @@ func (suite *BasicTestSuite) Test_Agent_Managed() {
 			},
 		},
 	}
-	err = controlPlaneClient.Create(ctx, &app, metav1.CreateOptions{})
-	requires.Nil(err)
+	err := suite.PrincipalClient.Create(suite.Ctx, &app, metav1.CreateOptions{})
+	requires.NoError(err)
 
 	key := fixture.ToNamespacedName(&app)
 
 	// Ensure the app has been pushed to the managed-agent
 	requires.Eventually(func() bool {
 		app := argoapp.Application{}
-		err := managedAgentClient.Get(ctx, key, &app, metav1.GetOptions{})
+		err := suite.ManagedAgentClient.Get(suite.Ctx, key, &app, metav1.GetOptions{})
 		return err == nil
-	}, 30*time.Second, 1*time.Second)
-
-	// Delete the app from the control-plane
-	err = controlPlaneClient.Delete(ctx, &app, metav1.DeleteOptions{})
-	requires.Nil(err)
-
-	// Ensure the app has been deleted from the managed-agent
-	requires.Eventually(func() bool {
-		app := argoapp.Application{}
-		err := managedAgentClient.Get(ctx, key, &app, metav1.GetOptions{})
-		return err != nil && errors.IsNotFound(err)
 	}, 30*time.Second, 1*time.Second)
 }
 
-func (suite *BasicTestSuite) Test_Agent_Autonomous() {
+func (suite *BasicTestSuite) Test_2() {
 	requires := suite.Require()
-
-	ctx := context.Background()
-
-	config, err := fixture.GetSystemKubeConfig("vcluster-control-plane")
-	requires.Nil(err)
-	controlPlaneClient, err := fixture.NewKubeClient(config)
-	requires.Nil(err)
-
-	config, err = fixture.GetSystemKubeConfig("vcluster-agent-autonomous")
-	requires.Nil(err)
-	autonomousAgentClient, err := fixture.NewKubeClient(config)
-	requires.Nil(err)
 
 	// Create an autonomous application on the autonomous-agent's cluster
 	app := argoapp.Application{
@@ -136,26 +100,120 @@ func (suite *BasicTestSuite) Test_Agent_Autonomous() {
 			},
 		},
 	}
-	err = autonomousAgentClient.Create(ctx, &app, metav1.CreateOptions{})
-	requires.Nil(err)
+	err := suite.AutonomousAgentClient.Create(suite.Ctx, &app, metav1.CreateOptions{})
+	requires.NoError(err)
 
 	key := types.NamespacedName{Name: app.Name, Namespace: "agent-autonomous"}
 
-	// Ensure the app has been pushed to the control-plane
+	// Ensure the app has been pushed to the principal
 	requires.Eventually(func() bool {
 		app := argoapp.Application{}
-		err := controlPlaneClient.Get(ctx, key, &app, metav1.GetOptions{})
+		err := suite.PrincipalClient.Get(suite.Ctx, key, &app, metav1.GetOptions{})
+		return err == nil
+	}, 30*time.Second, 1*time.Second)
+}
+
+func (suite *BasicTestSuite) Test_Agent_Managed() {
+	requires := suite.Require()
+
+	// Create a managed application in the principal's cluster
+	app := argoapp.Application{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "guestbook",
+			Namespace: "agent-managed",
+		},
+		Spec: argoapp.ApplicationSpec{
+			Project: "default",
+			Source: &argoapp.ApplicationSource{
+				RepoURL:        "https://github.com/argoproj/argocd-example-apps",
+				TargetRevision: "HEAD",
+				Path:           "kustomize-guestbook",
+			},
+			Destination: argoapp.ApplicationDestination{
+				Server:    "https://kubernetes.default.svc",
+				Namespace: "guestbook",
+			},
+			SyncPolicy: &argoapp.SyncPolicy{
+				SyncOptions: argoapp.SyncOptions{
+					"CreateNamespace=true",
+				},
+			},
+		},
+	}
+	err := suite.PrincipalClient.Create(suite.Ctx, &app, metav1.CreateOptions{})
+	requires.NoError(err)
+
+	key := fixture.ToNamespacedName(&app)
+
+	// Ensure the app has been pushed to the managed-agent
+	requires.Eventually(func() bool {
+		app := argoapp.Application{}
+		err := suite.ManagedAgentClient.Get(suite.Ctx, key, &app, metav1.GetOptions{})
+		return err == nil
+	}, 30*time.Second, 1*time.Second)
+
+	// Delete the app from the principal
+	err = suite.PrincipalClient.Delete(suite.Ctx, &app, metav1.DeleteOptions{})
+	requires.NoError(err)
+
+	// Ensure the app has been deleted from the managed-agent
+	requires.Eventually(func() bool {
+		app := argoapp.Application{}
+		err := suite.ManagedAgentClient.Get(suite.Ctx, key, &app, metav1.GetOptions{})
+		return err != nil && errors.IsNotFound(err)
+	}, 30*time.Second, 1*time.Second)
+}
+
+func (suite *BasicTestSuite) Test_Agent_Autonomous() {
+	requires := suite.Require()
+
+	// Create an autonomous application on the autonomous-agent's cluster
+	app := argoapp.Application{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "guestbook",
+			Namespace: "argocd",
+			Finalizers: []string{
+				"resources-finalizer.argocd.argoproj.io",
+			},
+		},
+		Spec: argoapp.ApplicationSpec{
+			Project: "default",
+			Source: &argoapp.ApplicationSource{
+				RepoURL:        "https://github.com/argoproj/argocd-example-apps",
+				TargetRevision: "HEAD",
+				Path:           "kustomize-guestbook",
+			},
+			Destination: argoapp.ApplicationDestination{
+				Server:    "https://kubernetes.default.svc",
+				Namespace: "guestbook",
+			},
+			SyncPolicy: &argoapp.SyncPolicy{
+				SyncOptions: argoapp.SyncOptions{
+					"CreateNamespace=true",
+				},
+			},
+		},
+	}
+	err := suite.AutonomousAgentClient.Create(suite.Ctx, &app, metav1.CreateOptions{})
+	requires.NoError(err)
+
+	key := types.NamespacedName{Name: app.Name, Namespace: "agent-autonomous"}
+
+	// Ensure the app has been pushed to the principal
+	requires.Eventually(func() bool {
+		app := argoapp.Application{}
+		err := suite.PrincipalClient.Get(suite.Ctx, key, &app, metav1.GetOptions{})
 		return err == nil
 	}, 30*time.Second, 1*time.Second)
 
 	// Delete the app from the autonomous-agent
-	err = autonomousAgentClient.Delete(ctx, &app, metav1.DeleteOptions{})
-	requires.Nil(err)
+	err = suite.AutonomousAgentClient.Delete(suite.Ctx, &app, metav1.DeleteOptions{})
+	requires.NoError(err)
 
-	// Ensure the app has been deleted from the control-plane
+	// Ensure the app has been deleted from the principal
 	requires.Eventually(func() bool {
 		app := argoapp.Application{}
-		err := controlPlaneClient.Get(ctx, key, &app, metav1.GetOptions{})
+		err := suite.PrincipalClient.Get(suite.Ctx, key, &app, metav1.GetOptions{})
 		return err != nil && errors.IsNotFound(err)
 	}, 30*time.Second, 1*time.Second)
 }
---
+++ test/e2e2/fixture/kubeconfig.go
@@ -0,0 +1,56 @@
+// Copyright 2024 The argocd-agent Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package fixture
+
+import (
+	"os"
+
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+// Retrieve the system-level Kubernetes config (e.g. ~/.kube/config)
+func GetSystemKubeConfig(kcontext string) (*rest.Config, error) {
+
+	overrides := clientcmd.ConfigOverrides{}
+	if len(kcontext) > 0 {
+		overrides.CurrentContext = kcontext
+	}
+
+	loadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
+	clientConfig := clientcmd.NewInteractiveDeferredLoadingClientConfig(loadingRules, &overrides, os.Stdin)
+
+	restConfig, err := clientConfig.ClientConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	err = setRateLimitOnRestConfig(restConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	return restConfig, nil
+}
+
+// setRateLimitOnRestConfig: set the QPS and Burst of the rest config
+func setRateLimitOnRestConfig(restConfig *rest.Config) error {
+	if restConfig != nil {
+		// Prevent rate limiting of our requests
+		restConfig.QPS = 100
+		restConfig.Burst = 250
+	}
+	return nil
+}
